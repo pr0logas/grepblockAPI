55d1e4acbb2f630531d02c804a53f6eaceecb5a1
Tomas Andriekus <tomas@dappradar.com>
2020-01-14 15:44:05 +0200

    Debug 26

--- serverFree.py
+++ serverFree.py
@@ -69,12 +69,14 @@ class GlobalSearch(Resource):
                 except:
                     workingData = res
                     test = re.search(rb'NumberLong', workingData)
+                    print(test + str('1'))
                     while test != None:
                         test = re.search(rb'NumberLong', workingData)
+                        print(test + str('2'))
                         timeSet = strftime("%Y-%m-%d %H:%M:%S", gmtime())
                         print(str(timeSet) + ' ***Failed to return JSON. Probably - "NumberLong" problem. Trying to reformat***')
                         numLong = re.search(rb'NumberLong.*?".*?"?"?\)', workingData)
-                        print(numLong)
+                        print(str(numLong) + str('3'))
                         resul = (numLong.group(0))
                         onlyDigits = (re.findall(rb'\d+', resul) [0])
                         final = (str(onlyDigits))
